a6c34aeaeda9f5cc6f71dfcf704b786a89e2bde0
Hectorjtt01 <hectorjtt@gmail.com>
2025-02-17 12:18:28 -0600

    Page2

---
+++ src/app/page2/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <main className="text-center mt-24">
+        <h1 className="text-5xl font-bold flex justify-center items-center h-screen">En construccion</h1>
+      </main>
+    </>
+  );
+}
